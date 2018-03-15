18e4caa6773c9ab83a7c39799db2db5232b50573
Germey <cqc@cuiqingcai.com>
2018-03-15 22:02:29 +0800

    Update spider.py

--- spider.py
+++ spider.py
@@ -7,7 +7,10 @@ import time
 
 def get_one_page(url):
     try:
-        response = requests.get(url)
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.162 Safari/537.36'
+        }
+        response = requests.get(url, headers=headers)
         if response.status_code == 200:
             return response.text
         return None
